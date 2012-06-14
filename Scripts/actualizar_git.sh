092973da7585bc8529e6e2527e37196ae76628cd
Fernando M <ferthedems@gmail.com>
2012-06-14 02:09:41 +0200

    Actualizamos con mejor soporte en el archivo de subida a github

--- Scripts/actualizar_git.sh
+++ Scripts/actualizar_git.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Script para actualizar el repositorio central del sistema,
-# alojado en github
-
+# This scripts allows you to connect your /home directory to a remote repository and
+# upload all changes automatically. Don't forget configure your .gitignore file.
 
 # @author: Fernando Moro
 # @date: 10 - June - 2012
@@ -10,34 +9,81 @@
 #        Soporte para la creación de nuevo repositorio, permitiendo conexión a un servidor nuevo.
 #        Incluir la ayuda de DIALOG, que se mostrará con el flag -h
 
-# Nos colocamos en el home
-cd $HOME
-echo "Moviendo al directorio $(pwd)"
-# Añadimos los cambios
-echo "Añadiendo cambios"
-git add .
-# Leemos el commit
-echo "Introduce el commit a añadir:"
-read COMMIT
-# Actualizamos...
+function addCommit ()
+{
+	# Go to /home 
+	cd $HOME
+	echo "Moving to: $(pwd)"
+	# Add changes
+	echo "Adding changes..."
+	git add .
+	# Reading commit
+	echo "Please, insert a new commit:"
+	read COMMIT
+	# Updating...
+	git commit -m "$COMMIT"
+	
+	# Uploading to GitHub...
+	echo "Choose a repository branch: <default: master>"
+	echo "Branches:"
+	echo "######################"
+	git branch
+	echo "######################"
+	read RAMA
+	
+	# Select a branch... 
+	if [ "$RAMA" != "" ]
+	then
+		echo "Uploading to GitHub, branch $RAMA..."
+		git push -u origin "$RAMA"
+	else
+		echo "You don't have chosen any branch, uploading repository to MASTER..."
+		git push -u origin master
+	fi
+}
 
-git commit -m "$COMMIT"
+function githelp
+{
+dialog --colors --backtitle "DEMS ® 2011" --title "GIT-COMMANDOS - BY DEMS" \
+--msgbox "\Zb\Z1INICIO \Zn
+$ git init
+$ git add .
+$ git commit -m <nombre>
+\Zb\Z1RESPALDOS POSTERIORES \Zn
+$ git commit -a -m <nombre2>
+\Zb\Z1COMPROBAR CAMBIOS \Zn
+$ git status
+\Zb\Z1VER RESPALDOS \Zn
+$ git log
+\Zb\Z1CARGAR RESPALDOS \Zn
+$ git checkout master
+$ git checkout SHA1_HASH
+$ git checkout :/'nombre'
+$ git checkout master~5 (5o estado hacia atras)
+$ git checkout SHA1_HASH <archivo> <archivo2>
+\Zb\Z1ETIQUETAS \Zn
+$ git tag <nombre> [<HASH commit>]
+\Zb\Z1CREAR RAMA \Zn
+$ git checkout -b <rama>
+\Zb\Z1BORRAR RAMA \Zn
+$ git branch -d <rama>
+\Zb\Z1LISTAR RAMAS \Zn
+$ git branch
+\Zb\Z1POSICIONARSE EN RAMA \Zn
+$ git checkout <rama>
+$ git checkout master (rama principal)
 
-# Lo subimos a GitHub
-echo "Ahora introduce la rama del repositorio: <default: master>"
-echo "Ramas disponibles:"
-echo "######################"
-git branch
-echo "######################"
-read RAMA
+" 0 0
+clear
 
-# Seleccionamos la rama
+}
 
-if [ "$RAMA" != "" ]
+
+
+if [ -d "$HOME/.git" ]
 then
-	echo "Subiendo el repositorio a GitHub, a la rama $RAMA..."
-	git push -u origin "$RAMA"
+	addCommit
 else
-	echo "No se ha elegido ninguna rama, subiendo a la rama MASTER..."
-	git push -u origin master
+	echo "You should initialice your repository."
+	exit 0
 fi
